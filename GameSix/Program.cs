2b0eec9a1768f0e60762c52ddfe0fdd25045bc6b
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-04-20 19:33:44 +0100

    Constructor

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -12,6 +12,15 @@ namespace GameSix // Note: actual namespace depends on the project name.
 
     public class Foe
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+        public Foe(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
