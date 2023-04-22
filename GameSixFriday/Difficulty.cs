eb03068cca0f2145c3df08e79192285175ad025a
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-04-22 13:50:27 +0100

    New Program File

---
+++ GameSixFriday/Difficulty.cs
@@ -0,0 +1,7 @@
+namespace GameSixFriday
+{
+    public enum Difficulty
+    {
+        
+    }
+}
---
+++ GameSixFriday/PowerUp.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GameSixFriday // Note: actual namespace depends on the project name.
+{
+    public enum PowerUp
+    {
+        Health,
+        Shield
+    }
+}
